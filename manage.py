0f7461dadf21a700106f3f2bbee6297186db0888
Kevin Conzo <kevinconzo@kevins-mbp.mynetworksettings.com>
2025-05-23 07:34:56 -0400

    Add manage.py for db upgrade on Render

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-# manage.py
-
 from flask.cli import FlaskGroup
-from main import app, db
+from main import app
+from models import db
 
 cli = FlaskGroup(app)
 
 if __name__ == "__main__":
     cli()
+
