114451b142b4898d4479ca0ef281c9b956c0b2b6
Kevin Conzo <kevinconzo@kevins-mbp.mynetworksettings.com>
2025-05-27 17:45:17 -0400

    🛠️ Full app cleanup and restructure

--- routes/resurgitag.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-def generate_resurgitag(base_name="user"):
-    tag = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-    return f"@{base_name.lower()}{tag}"
