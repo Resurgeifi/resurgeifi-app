f914b4928ecc45f2de299796587670764f3befc9
Kevin Conzo <kevinconzo@kevins-mbp.mynetworksettings.com>
2025-06-13 08:45:46 -0400

    Fix quest path for Render environment

--- utils/quest_loader.py
+++ utils/quest_loader.py
@@ -3,7 +3,7 @@ import os
 
 def load_quest(quest_id):
     """Loads quest data from YAML file based on quest_id."""
-    base_path = os.path.join("resurgeifi-app-main", "quests")
+    quest_path = os.path.join("quests", filename)
     filename = f"quest_{quest_id:02}.yaml"
     filepath = os.path.join(base_path, filename)
 
